45987e695beccd3d77383b38c6f785e6be1d0e9b
sudha <01fe24bca210@kletech.ac.in>
2025-11-12 02:47:49 -0800

    simpleinterest

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,27 +1,36 @@
-#simple_interest.py
-#Program to calculate simple interest
+# simple_interest.py
+# Program to calculate Simple Interest
+
 import sys
 
 def calculate_simple_interest(principal, rate, time):
-    """"Calculate simple interest given principal, rate, time."""
-    return (principal * rate * time) /100
+    """Calculate simple interest given principal, rate, and time."""
+    return (principal * rate * time) / 100
 
-if __name__ == "main":
-    print("=== Simple Interest Calculator===")
-try:
+if __name__== "__main__":
+    print("=== Simple Interest Calculator ===")
+
+    try:
+        # Case 1: Arguments passed through command line
         if len(sys.argv) == 4:
-        # Case 1: Arguments passed (for jenkins or CLI)
             p = float(sys.argv[1])
             r = float(sys.argv[2])
             t = float(sys.argv[3])
+
+        # Case 2: No arguments passed, take input from user
         else:
-        # Case 2: No arguments passed (for console use)    
             p = float(input("Enter the principal amount: "))
-            r = float(input("Enter the rate of the interest (%): "))
-            t = float(input("Enter the  time (in years): "))
+            r = float(input("Enter the rate of interest (%): "))
+            t = float(input("Enter the time (in years): "))
         print("===Program parameters ===")
-        print("Principal Amount :", p)
-        print("Rate of Interest :", r)
-        print("Time in years :", t)
-except ValueError:
-        print("Invalid input! Please enter numeric values. ")
+        print("princapal Amount:", p)
+        print("rate of Interest:", r)
+        print("Time in years:", t)
+        # Calculate and display the result
+        interest = calculate_simple_interest(p, r, t)
+        print(f"\nThe Simple Interest ={  interest :.2f}")
+
+    except ValueError:
+        print("Error: Please enter valid numeric values.")
+    except Exception as e:
+        print("An error occurred:", e)
